ae633752f30a5e799f2b09c813d8bfd281996088
Luca <silvester.luca13@gmail.com>
2022-02-17 21:21:31 -0500

    Add files via upload

---
+++ socketServer.py
@@ -0,0 +1,12 @@
+import socketserver
+
+class MyHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        while 1:
+            dataReceived = self.request.recv(20)
+            if not dataReceived: break
+            self.request.send(dataReceived)
+
+myServer = socketserver.ThreadingTCPServer(('',8888), MyHandler)
+myServer.serve_forever(  )
+
